aa3eaabf00d5ba546cdf83c1afa6746877006508
Adrien Gandon <riskou@users.noreply.github.com>
2017-10-24 01:01:37 +0200

    Add dev helper script

---
+++ chrandr-dev.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Helper script to execute Rename Episode main method
+
+"""
+
+import sys
+from chrandr import simple_gui
+
+if __name__ == '__main__':
+    sys.exit(simple_gui.main())
